baf8784fee60f784bd7e7125297eb7c5911b06c3
Blot Maxime <37617948+Eldiablot@users.noreply.github.com>
2018-04-06 09:40:05 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ let maPageWeb = document.querySelector('html')
 maPageWeb.onclick = function () {
    setTimeout(function(){alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
 }
+let monImage = FIREFOX.querySelector('img');
+
+monImage.onclick = function() {
+    let maSrc = monImage.getAttribute('src');
+    if(maSrc === 'images/firefox-icon.png') {
+      monImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      monImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+<script>
+  (function() {
+    var cx = '002987498994089218550:6irzm1z-jkc';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
