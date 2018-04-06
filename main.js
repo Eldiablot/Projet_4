7a18dc9e36cca067a5ca3059280e220edfc8153a
Blot Maxime <37617948+Eldiablot@users.noreply.github.com>
2018-04-06 08:09:56 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let monBouton = document.getElementById("bouton");
+let block = document.getElementById("corps_page")
+
+monBouton.onclick = function () {
+
+   if (corps_page.style.display = "none")
+   { 
+        monBouton.style.display = "none";
+        corps_page.style.display = "block";
+    } else {
+        block.display = "block";
+    }
+}
+
+
+let maPageWeb = document.querySelector('html')
+
+maPageWeb.onclick = function () {
+   setTimeout(function(){alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
+}
